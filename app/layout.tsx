8c3c38c55acfe3445e29298d28848c1ff2d08c64
brunoidalgo <idalgob99@gmail.com>
2025-03-17 07:04:33 -0300

    Adicionando Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,8 @@ export const metadata: Metadata = {
   title: "Marcondes Turismo",
 }
 
+import Script from 'next/script'
+
 export default function RootLayout({
   children,
 }: {
@@ -16,6 +18,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pr-br">
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Y12538YWKB"></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Y12538YWKB');`}
+        </Script>
+      </head>
       <body className={poppins.className}>{children}</body>
     </html>
   )
